84646da52d97098120206d937f3440c56a0d7e9d
unknown <naviworld98@gmail.com>
2020-09-26 20:18:17 +0530

    updated

---
+++ build/precache-manifest.a052168e608e81e5d1484f913f97d3cf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b72b44b4fc3f219b58d68ac16b9b28e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff7996e71fd3f7e1da1",
+    "url": "/static/css/main.e879c75c.chunk.css"
+  },
+  {
+    "revision": "7864b313690b4686c4e4",
+    "url": "/static/js/2.e2b23236.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.e2b23236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff7996e71fd3f7e1da1",
+    "url": "/static/js/main.a479a7bb.chunk.js"
+  },
+  {
+    "revision": "79f7867702e47af7632a",
+    "url": "/static/js/runtime-main.7efb7afb.js"
+  },
+  {
+    "revision": "832530f2496f33d68e9c96375d0303a8",
+    "url": "/static/media/a.832530f2.jpg"
+  },
+  {
+    "revision": "f43686c588c30dcf188ecd336433466e",
+    "url": "/static/media/b.f43686c5.jpg"
+  },
+  {
+    "revision": "20852c93d31c7d0dbe136afcdf1e0f8a",
+    "url": "/static/media/background.20852c93.png"
+  },
+  {
+    "revision": "def7a9b7684066f5863660b94dfde59c",
+    "url": "/static/media/c.def7a9b7.jpg"
+  },
+  {
+    "revision": "91e9a7be2120ce22ec5a9a9c34e80b44",
+    "url": "/static/media/co.91e9a7be.jpg"
+  },
+  {
+    "revision": "0b513786453dad04eb65823f6fdaac77",
+    "url": "/static/media/d.0b513786.jpg"
+  },
+  {
+    "revision": "4558bcb1aadcb50245ac98e511b3d406",
+    "url": "/static/media/f.4558bcb1.jpg"
+  },
+  {
+    "revision": "6a7fd99a11cff180165aa77c8279c8c6",
+    "url": "/static/media/i.6a7fd99a.jpg"
+  },
+  {
+    "revision": "d9c8605f35cecacaa6729ff07fba43f3",
+    "url": "/static/media/m1.d9c8605f.jpg"
+  }
+]);
